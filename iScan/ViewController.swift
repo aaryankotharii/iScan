2a029de748f00218e67c7963a161c4540113b17e
aaryankotharii <aaryan.kothari@gmail.com>
2020-05-03 11:14:37 +0530

    sessionSetup added

--- iScan/ViewController.swift
+++ iScan/ViewController.swift
@@ -7,14 +7,32 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
+    
+    let session: AVCaptureSession = AVCaptureSession()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    func sessionSetup(){
+        session.sessionPreset = AVCaptureSession.Preset.high
+        
+        if let device = AVCaptureDevice.default(for: .video ) {
+            do {
+                try session.addInput(AVCaptureDeviceInput(device: device))
+                
+            } catch {
+                print(error.localizedDescription)
+            }
+            
+            let previewLayer = AVCaptureVideoPreviewLayer(session: session)
+            previewLayer.frame = self.view.layer.bounds
+            self.view.layer.addSublayer(previewLayer)
+        }
+    }
 }
 
